d5a61af8b2dfb2fe1f5fb1f0f62eb31528d29c0f
Adam Biagianti <adam.biagianti@gsa.gov>
2018-12-18 16:07:45 -0800

    ideas for a wizard

--- src/app.js
+++ src/app.js
@@ -1,16 +1,51 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import Wizard from './components/wizard'
 import LocaleContext from './locale-context';
 
-export default class App extends React.Component {
+class App extends React.Component {
+  handleFormComplete = (values) => {
+    console.log('form done!')
+    this.props.history.push('/form/complete');
+  }
+
   render() {
     return (
       <LocaleContext.Consumer>
         {(t) => {
-          return (
-            <h1>placeholder</h1>
+          return (              
+            <Wizard title="Form" onDone={this.handleFormComplete}>
+              <Wizard.Section header="my header">
+                <Wizard.Step>
+                  <div className="margin-y-2">
+                    step 1
+                  </div>
+                </Wizard.Step>
+                <Wizard.Step>
+                  <div className="margin-y-2">
+                    step 2
+                  </div>
+                </Wizard.Step>
+                <Wizard.Step>
+                  <div className="margin-y-2">
+                    step 3
+                  </div>
+                </Wizard.Step>
+              </Wizard.Section>
+
+              <Wizard.Section>
+                <Wizard.Step>
+                  <div className="margin-y-2">
+                    This is the first step of section 2
+                  </div>
+                </Wizard.Step>
+              </Wizard.Section>
+            </Wizard>
           );
         }}
       </LocaleContext.Consumer>
     );
   }
 }
+
+export default withRouter(App);
---
+++ src/components/wizard/index.js
@@ -0,0 +1,191 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Formik } from 'formik';
+import Debug from './debug';
+
+const WizardContext = React.createContext();
+
+class Section extends React.Component {
+  static propTypes = {
+    header: PropTypes.string,
+    initialValues: PropTypes.object
+  }
+
+  state = {
+    step: 0
+  }
+
+  next = values =>
+  this.setState(state => ({
+    step: Math.min(state.step + 1, this.props.children.length - 1),
+    values
+  }));
+
+  getActiveStep() {
+    return React.Children.toArray(this.props.children)[this.state.step];
+  }
+
+  render() {
+    const activeStep = this.getActiveStep();
+    const isLastStep = this.state.step === React.Children.count(this.props.children) - 1;
+
+    return (
+      <section>
+        <h2>{this.props.header}</h2>
+        <Formik
+          initialValues={this.props.initialValues}
+          onSubmit={this.handleSubmit}
+          render={({ values, handleSubmit, isSubmitting, handleReset }) => {
+            return (
+              <div>
+                { activeStep }
+                <button
+                  type="button"
+                  className="usa-button usa-button-primary"
+                  onClick={isLastStep ? this.props.handleSubmit : this.next}
+                >
+                  next
+                </button>
+              </div>
+            );
+          }}
+        />
+      </section>
+    );
+  }
+};
+
+class Step extends React.Component {
+  static propTypes = {
+    header: PropTypes.string,
+    // initialValues: PropTypes.object.isRequired,
+    onSubmit: PropTypes.func,
+    validate: PropTypes.func
+  }
+
+  static defaultProps = {
+    children: null
+  }
+
+  handleSubmit = (/** values i guess? */) => {
+    //this.validate() // is this async?
+    this.props.onSubmit() && this.props.onSubmit();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {this.props.children}
+      </form>
+    )
+  }
+}
+
+class Progress extends React.Component {
+  static propTypes = {
+    step: PropTypes.number,
+    steps: PropTypes.number,
+  }
+  
+  render() {
+    return (
+      <div id="progress">
+        { `Step ${this.props.step} of ${this.props.steps}` }
+      </div>
+    );
+  }
+}
+
+class Wizard extends React.Component {
+  static Step = Step
+  static Section = Section
+  static Progress = Progress
+  static Context = WizardContext.Consumer
+  static propTypes = {
+    onDone: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func,
+    title: PropTypes.string,
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      step: 0,
+      initialValues: props.initialValues
+    };
+  }
+
+  next = (values) =>
+    this.setState(state => ({
+      step: Math.min(state.step + 1, this.getChildCount()),
+      values
+    }));
+
+  validate = (values) => {
+    console.log('validate hook called')
+    const activeStep = this.getActiveStep();
+
+    // call the validate method of the child step, if there is one
+    return activeStep.props.validate ? activeStep.props.validate(values) : {};
+  }
+
+  handleSubmit = (values, bag) => {
+    console.log('submit hook called');
+
+    const { onSubmit } = this.props;
+
+    onSubmit && onSubmit(values);
+
+    !this.isLastPage() ? this.next(values) : this.props.onDone(values);
+  }
+
+  getActiveStep() {
+    return React.Children.toArray(this.props.children)[this.state.step];
+  }
+
+  getChildCount() {
+    return React.Children.count(this.props.children);
+  }
+
+  isLastPage() {
+    return this.state.step + 1 === this.getChildCount();
+  }
+
+  render() {
+    const activeStep = this.getActiveStep();
+
+    const providedState = {
+      ...this.state,
+      ...this.props,
+      advanceSection: this.next,
+    };
+
+    return (
+      <WizardContext.Provider value={providedState}>
+        <h2>{this.props.title}</h2>
+        <Wizard.Progress step={this.state.step + 1} steps={this.getChildCount()} />
+        <Formik
+          initialValues={this.props.initialValues}
+          onSubmit={this.handleSubmit}
+          validate={this.validate}
+          render={({ values, handleSubmit, isSubmitting, handleReset }) => {
+            return (
+              <div>
+                {
+                  React.cloneElement(activeStep, {
+                    handleSubmit: handleSubmit,
+                    ...activeStep.props
+                  })
+                }
+              </div>
+            );
+          }}
+        />
+        <Debug />
+      </WizardContext.Provider>
+    );
+  }
+}
+
+export default Wizard;
