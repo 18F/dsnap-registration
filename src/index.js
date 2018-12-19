4bd3c915c58106273e6a290c7602e55c29a72df5
Adam B <el-mapache@users.noreply.github.com>
2018-12-19 08:42:51 -0800

    Adds i18n and locale provider (#15)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'uswds/dist/css/uswds.css';
 import { BrowserRouter } from 'react-router-dom';
+import { withNamespaces } from 'react-i18next';
+import 'uswds/dist/css/uswds.css';
 import Routes from './routes';
+import './i18n';
+
+const LocalizedRoutes = withNamespaces()(Routes);
 
 ReactDOM.render(
   <BrowserRouter>
-    <Routes />
+    <LocalizedRoutes />
   </BrowserRouter>,
   document.getElementById('root')
 );
