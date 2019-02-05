f843b3799d32ff1192111877bd5cf3ed7d2b8c6b
Adam Biagianti <adam.biagianti@gsa.gov>
2019-02-05 12:53:13 -0800

    remove link

--- src/pages/form/steps/offramp.js
+++ src/pages/form/steps/offramp.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import Wizard from 'components/wizard';
 import withLocale from 'components/with-locale';
 import UI from 'components/ui';
