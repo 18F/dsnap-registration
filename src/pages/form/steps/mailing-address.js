5bc340ad35f7aaadd8af359912597548bc939eff
Adam B <el-mapache@users.noreply.github.com>
2019-02-05 10:34:26 -0800

    Adds mailing address page (#25)

---
+++ src/pages/form/steps/mailing-address.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { buildNestedKey } from 'utils';
+import withLocale from 'components/with-locale';
+import Wizard from 'components/wizard';
+import FormikField from 'components/formik-field';
+
+class MailingAddress extends React.Component {
+  static modelName = 'mailingAddress'
+  static tKey = 'addresses'
+
+  static propTypes = {
+    handleChange: PropTypes.func,
+    registerStep: PropTypes.func,
+    sectionName: PropTypes.string,
+  }
+
+  render() {
+    const { handleChange, sectionName, t } = this.props;
+    const { modelName, tKey } = MailingAddress;
+
+    return (
+      <Wizard.Step
+        header={t(`${buildNestedKey(sectionName, modelName)}.header`)}
+        modelName={modelName}
+        registerStep={this.props.registerStep}
+      >
+        <FormikField
+          name={`${sectionName}.${modelName}.street1`}
+          onChange={handleChange}
+          labelText={t(`${buildNestedKey(sectionName, tKey, 'street1', 'label')}`)}
+        />
+        <FormikField
+          name={`${sectionName}.${modelName}.street2`}
+          onChange={handleChange}
+          labelText={t(`${buildNestedKey(sectionName, tKey, 'street2', 'label')}`)}
+        />
+        <FormikField
+          name={`${sectionName}.${modelName}.city`}
+          onChange={handleChange}
+          labelText={t(`${buildNestedKey(sectionName, tKey, 'city', 'label')}`)}
+        />
+        <FormikField
+          name={`${sectionName}.${modelName}.state`}
+          onChange={handleChange}
+          labelText={t(`${buildNestedKey(sectionName, tKey, 'state', 'label')}`)}
+        />
+        <FormikField
+          name={`${sectionName}.${modelName}.zip`}
+          onChange={handleChange}
+          type="number"
+          labelText={t(`${buildNestedKey(sectionName, tKey, 'zip', 'label')}`)}
+        />
+      </Wizard.Step>
+    )
+  }
+}
+
+export default withLocale(MailingAddress);
+
