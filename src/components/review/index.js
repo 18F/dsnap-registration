e43594614d3968c23cc932ddea0cddd36d55b0f3
Adam Biagianti <adam.biagianti@gsa.gov>
2019-02-28 16:15:04 -0800

    Adds Review table components

--- src/components/review/index.js
+++ src/components/review/index.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import withLocale from 'components/with-locale';
 import Button from 'components/button';
 
@@ -11,7 +12,31 @@ const ToggleButton = ({ activeText, inactiveText, isActive, ...rest }) => (
   >
     { isActive ? activeText : inactiveText }
   </Button>
-)
+);
+
+const ReviewHeader = ({ children }) => (
+  <h3
+    className="margin-0 grid-col-fill text-bottom display-inline-block"
+    style={{ alignSelf: 'flex-end' }}
+  >
+    { children }
+  </h3>
+);
+
+const ReviewTableRow = ({ name, data, stripe }) => (
+  <div className="grid-row margin-y-2">
+    <div className={classnames('grid-col padding-2', { 'bg-base-lighter': stripe })}>
+      <b>{ name }:</b>
+      <p>{ data }</p>
+    </div>
+  </div>
+);
+
+const ReviewTable = ({ rows }) => (
+  <div>
+    { rows.map((row, index) => <ReviewTableRow {...row} stripe={index % 2 === 0} />) }
+  </div>
+);
 
 class Review extends React.Component {
   static propTypes = {
@@ -36,22 +61,26 @@ class Review extends React.Component {
 
     return (
       <React.Fragment>
-        <div className="border-bottom-1px border-base-lighter">        
+        <div className="border-bottom-1px border-base-lighter">
           <div className="grid-row margin-bottom-05">
-            <h3
-              className="margin-0 grid-col-fill text-bottom display-inline-block"
-              style={{ alignSelf: 'flex-end' }}
-            >
-              {t(`review.sections.${this.props.name}`)}
-            </h3>
+            <ReviewHeader>
+              { t(`review.sections.${this.props.name}`) }
+            </ReviewHeader>
             <ToggleButton
               activeText={t('review.edit.active')}
               inactiveText={t('review.edit.inactive')}
               isActive={this.state.editing}
               onClick={this.handleToggleEdit}
+              className="margin-right-0"
             />
           </div>
         </div>
+        <ReviewTable
+          rows={[
+            { name: 'Name', data: 'hey' },
+            { name: 'Other', data: 'wheee' }
+          ]}
+        />
         <ToggleButton
           activeText={t('review.details.active')}
           inactiveText={t('review.details.inactive')}
