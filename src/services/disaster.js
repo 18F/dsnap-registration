ed5bbed8ea73aba3f7324b5ce21c78d7a4817038
Adam Biagianti <adam.biagianti@gsa.gov>
2019-03-18 14:59:39 -0700

    Update disaster endpoint for staging/prod

--- src/services/disaster.js
+++ src/services/disaster.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const endpoint = 'disasters';
 const location = process.env.NODE_ENV === 'development' ?
-  'http://localhost:8000' : '';
+  'http://localhost:8000' : 'https://dsnap-rules-staging.app.cloud.gov';
 
   // TODO: here we should xform the data into a keyed dictionary,
   // relying on the index of an array is pretty sketchy
