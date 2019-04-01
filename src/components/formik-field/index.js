98fc68bb032a1c5112b70a8d9ab5097d8d0d7870
Adam Biagianti <adam.biagianti@gsa.gov>
2019-04-01 16:44:06 -0700

    Fix some validation issues
    * Fix bug where dates with years of more than 4 digits were valid
    
    * Fix bug where ssn of length zero was triggering validations
    
    * Fix bug where all errors were getting cleared when a single field was
    updated in a submitted form with multiple errors

--- src/components/formik-field/index.js
+++ src/components/formik-field/index.js
@@ -72,7 +72,7 @@ class FormikField extends React.Component {
                 onInput={() => form.setFieldTouched(name, true, true)}
                 {...rest}
               />
-              { this.props.showError && getIn(form.touched, name) ? <FormikError name={name} /> : null }
+              { this.props.showError && (form.submitCount || getIn(form.touched, name)) ? <FormikError name={name} /> : null }
             </React.Fragment>
           );
         }}
