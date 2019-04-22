7454299978720afb0534eb3f1357ad150f1443cf
Adam Biagianti <adam.biagianti@gmail.com>
2019-04-22 13:13:41 -0400

    Clear zeros from currency field on focus

--- src/components/currency-input/index.js
+++ src/components/currency-input/index.js
@@ -2,6 +2,27 @@ import React from 'react';
 import FormikField from 'components/formik-field';
 
 class CurrencyInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+  }
+
+  handleFocus = (event, _, { setFieldValue }) => {
+    const { value } = event.target;
+
+    if (Number(value) === 0) {
+      setFieldValue(this.props.name, '', false);
+    }
+  }
+
+  handleBlur = (_, field, { setFieldValue }) => {
+    if (field.value === '') {
+      setFieldValue(this.props.name, 0, true);
+    }
+  }
+
   handleChange = (event) => {
     const { name, value } = event.target;
 
@@ -16,6 +37,8 @@ class CurrencyInput extends React.Component {
         groupClassName="currency-field"
         prefix="$"
         onChange={this.handleChange}
+        onBlur={this.handleBlur}
+        onFocus={this.handleFocus}
         {...rest}    
       />
     );
