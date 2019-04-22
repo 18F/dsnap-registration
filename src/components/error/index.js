2b484c390eeb37ccd132a7ffc2b43b72a97e8819
Adam B <el-mapache@users.noreply.github.com>
2019-04-22 17:02:45 -0400

    Restore app routes file (#110)
    * Something is coupled to formik in a way that breaks errors when the routes
    are moved

--- src/components/error/index.js
+++ src/components/error/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-  message: PropTypes.oneOf([
+  message: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.object,
   ]).isRequired,
