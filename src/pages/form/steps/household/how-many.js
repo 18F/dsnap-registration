d8d3428f8ef92262c2532276c3aabfa5e27d00d1
Adam B <el-mapache@users.noreply.github.com>
2019-04-25 17:15:06 -0400

    (WIP) Improves household members UI + Copy (#123)
    Improves household members UI + Copy

--- src/pages/form/steps/household/how-many.js
+++ src/pages/form/steps/household/how-many.js
@@ -12,31 +12,50 @@ const addToHousehold = household => values => {
   const nextMemberCount = Number(values.household.numMembers);
 
   return {
-    household: { ...updateHouseholdMembers(household, nextMemberCount) }
+    household: { ...updateHouseholdMembers(household, nextMemberCount - 1) }
   };
 };
 
-const HowMany = ({ handleChange, sectionName, t, registerStep }) =>
-  <Wizard.Context>
-    { ({ household, basicInfo, disasters }) => (
-      <Wizard.Step
-        header={t(`${buildNestedKey(sectionName, modelName, 'header')}`)}
-        modelName='numMembers'
-        registerStep={registerStep}
-        validationSchema={householdCountSchema}
-        onNext={addToHousehold(household)}
-      >
-        <FormikField
-          labelText={t(buildNestedKey(sectionName, modelName, 'label'), {
-            benefitStartDate: getBeginDate(getDisaster(disasters, basicInfo.disasterId))
-          })}
-          explanation={t(buildNestedKey(sectionName, modelName, 'explanation'))}
-          onChange={handleChange}
-          name={`${sectionName}.numMembers`}
-          className="desktop:grid-col-1"
-        />
-      </Wizard.Step>
-    )}
-  </Wizard.Context>
+class HowMany extends React.Component {
+  buildOptions() {
+    const { t } = this.props;
+    const initial = [{ text: t('household.count.justMeOption'), value: 1 }]
 
+    return initial.concat(Array.apply(null, { length: 12 })
+      .map((_, index) => ({
+        text: `${index + 2} ${t('general.people')}`,
+        value: index + 2
+      })));
+  }
+
+  render() {
+    const { handleChange, sectionName, t, registerStep } = this.props;
+
+    return (
+      <Wizard.Context>
+        { ({ household, basicInfo, disasters }) => (
+          <Wizard.Step
+            header={t(`${buildNestedKey(sectionName, modelName, 'header')}`)}
+            modelName='numMembers'
+            registerStep={registerStep}
+            validationSchema={householdCountSchema}
+            onNext={addToHousehold(household)}
+          >
+            <FormikField
+              labelText={t(buildNestedKey(sectionName, modelName, 'label'), {
+                benefitStartDate: getBeginDate(getDisaster(disasters, basicInfo.disasterId))
+              })}
+              explanation={t(buildNestedKey(sectionName, modelName, 'explanation'))}
+              onChange={handleChange}
+              name={`${sectionName}.numMembers`}
+              className="desktop:grid-col-1"
+              type="select"
+              options={this.buildOptions()}
+            />
+          </Wizard.Step>
+        )}
+      </Wizard.Context>
+    );
+  }
+}
 export default withLocale(HowMany);
